1ce4e58655f435e032f3d2c19381a791daecb280
mayur.borse <mayur.borse@nxglabs.com>
2019-04-27 02:54:16 +0530

    same url generates same id, redirection works

--- shortenApp/urls.py
+++ shortenApp/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('short_id', views.redirect_original, name='redirectoriginal'),
-    path('makeshort', views.shorten_url, name='shortenurl'),
+    path('id', views.get_short_code, name='id'),
+    path('<str:short_id>', views.redirect_original, name='redirectoriginal'),
+    path('makeshort/', views.shorten_url, name='shortenurl'),
+    #re_path(r'^(?P&lt;short_id&gt;\w{6})$',views.redirect_original, name='redirectoriginal'),
+
 ]
--- shortenApp/views.py
+++ shortenApp/views.py
@@ -12,55 +12,36 @@ def index(request):
     return render(request, 'shortenApp/index.html', c)
  
 def redirect_original(request, short_id):
+    print(short_id)
     url = get_object_or_404(Urls, pk=short_id) # get object, if not        found return 404 error
+    print(url)
     url.count += 1
     url.save()
     return HttpResponseRedirect(url.httpurl)
-
-def shortenUrl(request):
-    url = request.POST.get("url", "")
-    print (url)
-    try:
-        urll = Urls.objects.get(httpurl=url)
-        short_id = urll.short_id
-    except (Urls.DoesNotExist):
-        print ("Does not exist")
-    if not (url == ""):
-        print ("URL not empty !")
-        short_id = getShortCode()
-        print ("Got shortcode")
-        b = Urls(httpurl=url, short_id=short_id)
-        b.save()
-        print ("Saved URL")
-        response_data = {}
-        response_data['url'] = "/" + short_id
-    else:
-        return HttpResponse(json.dumps({"error": "error occurs"}),content_type="application/json")
-    response_data = {}
-    response_data['url'] = "/" + short_id
-    return HttpResponse(json.dumps(response_data), content_type="application/json")
-
-
-
 def shorten_url(request):
     url = request.POST.get("url",'') 
-    #if not Url.objects.filter(pk=short_id).exists(): return short_id
-    if not (url == ''):
-        short_id = get_short_code()
-        b = Urls(httpurl=url, short_id=short_id)
-        b.save()
- 
+    print(url)
+    if  Urls.objects.filter(httpurl=url).exists(): 
+        short_id = Urls.objects.get(httpurl=url).short_id
         response_data = {}
         response_data['url'] = settings.SITE_URL + "/" + short_id
         return HttpResponse(json.dumps(response_data),  content_type="application/json")
-    return HttpResponse(json.dumps({"error": "error occurs"}), content_type="application/json")
+    elif not (url == ''):
+        short_id = get_short_code()
+        b = Urls(httpurl=url, short_id=short_id)
+        b.save()
+        response_data = {}
+        response_data['url'] = settings.SITE_URL + "/" + short_id
+        return HttpResponse(json.dumps(response_data),  content_type="application/json")
+    if url =='':
+        return HttpResponse(json.dumps({"error": "error occurs"}), content_type="application/json")
  
 def get_short_code():
     length = 6
     char = string.ascii_uppercase + string.digits + string.ascii_lowercase
     # if the randomly generated short_id is used then generate next
     while True:
-        if not Url.objects.filter(pk=short_id).exists(): return short_id
+        #if not Urls.objects.filter(pk=short_id).exists(): return short_id
         short_id = ''.join(random.choice(char) for x in range(length))
         try:
             temp = Urls.objects.get(pk=short_id)
