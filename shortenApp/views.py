e4e8cc5872f1501f63cd6821c5d1f203d9e39254
mayur.borse <mayur.borse@nxglabs.com>
2019-04-28 18:46:44 +0530

    completed with expiry timer(JS)

--- shortenApp/views.py
+++ shortenApp/views.py
@@ -4,8 +4,8 @@ import random, string, json
 from shortenApp.models import Urls
 from django.http import HttpResponseRedirect, HttpResponse
 from django.conf import settings
-#from django.core.context_processor import csrf
-#from django.views.decorators import csrf
+import time
+start = time.time()
 def index(request):
     c = {}
     #c = RequestContext(request)
@@ -13,27 +13,41 @@ def index(request):
     return render(request, 'shortenApp/index.html', c)
  
 def redirect_original(request, short_id):
-    url = get_object_or_404(Urls, pk=short_id) # get object, if not        found return 404 error
-    url.count += 1
+    end=time.time()
+    print("END: ", end)
+    expiry = end - start
+    print("EXPIRY: ",expiry)
+    if expiry>30.0:
+        print('link expired')
+        url = Urls.objects.filter(pk=short_id).delete()
+        expiry = 0.0
+        end = 0.0
+    url = get_object_or_404(Urls, pk=short_id) # get object, if not found return 404 error
+    url.count += 11
     url.save()
     return HttpResponseRedirect(url.httpurl)
 def shorten_url(request):
+    global start
     url = request.POST.get("url",'') 
-    if  Urls.objects.filter(httpurl=url).exists(): 
-        short_id = Urls.objects.get(httpurl=url).short_id
-        response_data = {}
-        response_data['url'] = settings.SITE_URL + "/" + short_id
-        return HttpResponse(json.dumps(response_data),  content_type="application/json")
-    elif not (url == ''):
-        short_id = get_short_code()
-        b = Urls(httpurl=url, short_id=short_id)
-        b.save()
-        response_data = {}
-        response_data['url'] = settings.SITE_URL + "/" + short_id
-        return HttpResponse(json.dumps(response_data),  content_type="application/json")
-    if url =='':
+    if not (url == ''):
+        start = time.time()
+        print("START: ", start)
+        if  Urls.objects.filter(httpurl=url).exists(): 
+            print('URL exists... fetching URl...')
+            short_id = Urls.objects.get(httpurl=url).short_id
+            response_data = {}
+            response_data['url'] = settings.SITE_URL + "/" + short_id
+            return HttpResponse(json.dumps(response_data),  content_type="application/json")
+        else:
+            print('generating URL')
+            short_id = get_short_code()
+            b = Urls(httpurl=url, short_id=short_id)
+            b.save()
+            response_data = {}
+            response_data['url'] = settings.SITE_URL + "/" + short_id
+            return HttpResponse(json.dumps(response_data),  content_type="application/json")
+    elif url =='':
         return HttpResponse(json.dumps({"error": "error occurs"}), content_type="application/json")
- 
 def get_short_code():
     length = 6 
     char = string.ascii_uppercase + string.digits + string.ascii_lowercase
